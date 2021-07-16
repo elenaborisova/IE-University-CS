ede1b473cd16fd3b6e5a5c48dd032045f21bacd2
Elena Borisova <elenaborrisova@gmail.com>
2021-07-16 14:09:46 +0200

    add data manipulation exercises with pandas

---
+++ Coding Principles/14-pandas/dicts.py
@@ -0,0 +1,13 @@
+def main():
+    this_dict = {
+        'brand': 'Ford',
+        'model': 'Mustang',
+        'year': 1964,
+    }
+
+    print(this_dict['brand'])
+    print(this_dict['model'])
+    print(this_dict['year'])
+
+
+main()
